8df2a57d6071818ffc599879fd5f88ff6db84a60
Ricardo <lrsj2003@gmail.com>
2023-04-10 22:19:12 -0300

    a

---
+++ Lista01/OlaMundo.c
@@ -0,0 +1,5 @@
+int main(int argc, char const *argv[])
+{
+    printf("Ola Mundo\n");
+    return 0;
+}
---
+++ Lista01/Pedagio.c
@@ -0,0 +1,6 @@
+int main(int argc, char const *argv[])
+{
+    int l, d;
+    int k, p;
+    return 0;
+}
